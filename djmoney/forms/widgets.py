6e34e2b80df6e845533128d7e6356d2d296677a6
Mario César Señoranis Ayala <mariocesar@creat1va.com>
2012-04-11 02:44:50 +0100

    PEP8ify

--- djmoney/forms/widgets.py
+++ djmoney/forms/widgets.py
@@ -8,16 +8,18 @@ __all__ = ('InputMoneyWidget', 'CurrencySelectWidget',)
 CURRENCY_CHOICES = [(c.code, c.name) for i, c in CURRENCIES.items() if c.code != DEFAULT_CURRENCY_CODE]
 CURRENCY_CHOICES.sort(key=operator.itemgetter(1))
 
+
 class CurrencySelectWidget(forms.Select):
     def __init__(self, attrs=None, choices=CURRENCY_CHOICES):
         super(CurrencySelectWidget, self).__init__(attrs, choices)
-    
+
+
 class InputMoneyWidget(forms.TextInput):
-    
+
     def __init__(self, attrs=None, currency_widget=None):
         self.currency_widget = currency_widget or CurrencySelectWidget()
         super(InputMoneyWidget, self).__init__(attrs)
-    
+
     def render(self, name, value, attrs=None):
         amount = ''
         currency = ''
@@ -30,8 +32,8 @@ class InputMoneyWidget(forms.TextInput):
         if isinstance(value, int) or isinstance(value, Decimal):
             amount = value
         result = super(InputMoneyWidget, self).render(name, amount)
-        result += self.currency_widget.render(name+'_currency', currency)
+        result += self.currency_widget.render(name + '_currency', currency)
         return result
-    
+
     def value_from_datadict(self, data, files, name):
-        return (data.get(name, None), data.get(name+'_currency', None))
+        return (data.get(name, None), data.get(name + '_currency', None))
