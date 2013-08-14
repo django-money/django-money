f860f7b5b064ab3ef4fd829d67318b55a9fe80a8
Wojciech Banaś <fizista@gmail.com>
2013-08-14 07:59:40 +0200

    fix MoneyPath tests

--- djmoney/tests/money_patched.py
+++ djmoney/tests/money_patched.py
@@ -1,5 +1,11 @@
 # -*- encoding: utf-8
+from  moneyed import test_moneyed_classes
+from djmoney.models.fields import MoneyPatched
 
-from  moneyed.test_moneyed_classes import *
-from djmoney.models.fields import MoneyPatched as Money
+# replace class "Money" a class "MoneyPath"
+test_moneyed_classes.Money = MoneyPatched
+
+TestCurrency = test_moneyed_classes.TestCurrency
+
+TestMoney = test_moneyed_classes.TestMoney
 
