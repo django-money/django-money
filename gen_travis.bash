3a5304d8fbf204e97f6d3f3f81d46e42669bcdec
aph <a.hewson@gmail.com>
2013-11-18 12:20:14 +0000

    cut down the travis envs

--- gen_travis.bash
+++ gen_travis.bash
@@ -9,8 +9,18 @@ script: tox -e $ENV
 env:
 END
 
-tox --list | (
-  while read i ; do
-    echo "  - ENV=$i"
-  done
-)
+if [[ $1 == "--all" ]] ; then
+  tox --list | (
+    while read i ; do
+      echo "  - ENV=$i"
+    done
+  )
+else
+  # a few representative envs...
+  cat <<"END"
+  - ENV=py27-django15-pmlatest
+  - ENV=py27-django16-pmlatest
+  - ENV=py33-django15-pmpython3
+  - ENV=py33-django16-pmpython3
+END
+fi
