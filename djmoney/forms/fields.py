33ce6ef8bcd7507847b5c23198b909072bd53d94
Johannes Hoppe <info@johanneshoppe.com>
2013-12-16 21:05:53 +0100

    changes: to MultiValueField instead of DecimalField
    This is a more ‘django’-like approach. I use existing fields and there
    widgets rather than creating them all.
    This works well and reduces the code size and intreduces real l10n.

--- djmoney/forms/fields.py
+++ djmoney/forms/fields.py
@@ -1,69 +1,19 @@
 from __future__ import unicode_literals
-import operator
-from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
-from django import forms
-from .widgets import InputMoneyWidget
-from moneyed.classes import Money, CURRENCIES, DEFAULT_CURRENCY_CODE
+from django.forms import MultiValueField, DecimalField, ChoiceField
+from .widgets import MoneyWidget, CURRENCY_CHOICES
+from moneyed.classes import Money
 
 __all__ = ('MoneyField',)
 
-PROJECT_CURRENCIES = getattr(settings, 'CURRENCIES', None)
 
-if PROJECT_CURRENCIES:
-    CURRENCY_CHOICES = [(code, CURRENCIES[code].name) for code in
-                        PROJECT_CURRENCIES]
-else:
-    CURRENCY_CHOICES = [(c.code, c.name) for i, c in CURRENCIES.items() if
-                        c.code != DEFAULT_CURRENCY_CODE]
+class MoneyField(MultiValueField):
+    def __init__(self, currency_widget=None, currency_choices=CURRENCY_CHOICES, max_value=None, min_value=None,
+                 max_digits=None, decimal_places=None, *args, **kwargs):
+        decimal_field = DecimalField(max_value, min_value, max_digits, decimal_places, *args, **kwargs)
+        self.widget = currency_widget if currency_widget else MoneyWidget(choices=CURRENCY_CHOICES,
+                                                                          amount_widget=decimal_field.widget)
+        fields = (decimal_field, ChoiceField(choices=currency_choices))
+        super(MoneyField, self).__init__(fields, *args, **kwargs)
 
-CURRENCY_CHOICES.sort(key=operator.itemgetter(1))
-
-
-class MoneyField(forms.DecimalField):
-    def __init__(self, currency_widget=None, currency_choices=CURRENCY_CHOICES, localize=False,
-                 *args, **kwargs):
-        widget = InputMoneyWidget(currency_widget=currency_widget,
-                                  currency_choices=currency_choices)
-        self.localize = localize
-        if self.localize:
-            widget.is_localized = True
-            widget.input_type = 'text'
-        kwargs.setdefault('widget', widget)
-        super(MoneyField, self).__init__(*args, **kwargs)
-
-    def to_python(self, value):
-
-        if value is None:
-            return None
-        if isinstance(value, Money):
-            return value
-
-        if not isinstance(value, tuple) or len(value) != 2:
-            raise Exception(
-                "Invalid money input, expected amount and currency, got: %s." % value)
-
-        amount, currency = value
-        if not currency:
-            raise forms.ValidationError(_('Currency is missing'))
-
-        if not isinstance(currency, basestring):
-            raise forms.ValidationError(
-                _("Unrecognized currency type '%s'." % currency))
-
-        currency = currency.upper()
-        if currency not in CURRENCIES or currency == DEFAULT_CURRENCY_CODE:
-            raise forms.ValidationError(
-                _("Unrecognized currency type '%s'." % currency))
-
-        amount = super(MoneyField, self).to_python(amount)
-        return Money(amount=amount, currency=currency)
-
-    def validate(self, value):
-        if value is None:
-            return None
-        if not isinstance(value, Money):
-            raise Exception(
-                "Invalid money input, expected Money object to validate.")
-
-        super(MoneyField, self).validate(value.amount)
+    def compress(self, data_list):
+        return Money(*data_list[:2])
