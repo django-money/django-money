00f7c9bed1f098a5871d67d4be0135720758932f
Benjamin Bach <benjaoming@gmail.com>
2015-09-20 00:10:03 +0200

    Global setting for default currency, DEFAULT_CURRENCY #109

--- djmoney/forms/widgets.py
+++ djmoney/forms/widgets.py
@@ -1,21 +1,10 @@
-import operator
-
 from django.conf import settings
 from django.forms import TextInput, Select, MultiWidget
 from moneyed import CURRENCIES, DEFAULT_CURRENCY_CODE
+from ..settings import CURRENCY_CHOICES
+
 
 __all__ = ('MoneyWidget', )
-
-PROJECT_CURRENCIES = getattr(settings, 'CURRENCIES', None)
-
-if PROJECT_CURRENCIES:
-    CURRENCY_CHOICES = [(code, CURRENCIES[code].name) for code in
-                        PROJECT_CURRENCIES]
-else:
-    CURRENCY_CHOICES = [(c.code, c.name) for i, c in CURRENCIES.items() if
-                        c.code != DEFAULT_CURRENCY_CODE]
-
-CURRENCY_CHOICES.sort(key=operator.itemgetter(1))
 
 
 class MoneyWidget(MultiWidget):
