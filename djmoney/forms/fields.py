f49788b3da61fcfd9998623b0d74c327d966c323
Benjamin Bach <benjaoming@gmail.com>
2015-09-20 13:44:39 +0200

    Fix #97 - use choices setting for currency choices

--- djmoney/forms/fields.py
+++ djmoney/forms/fields.py
@@ -5,7 +5,8 @@ from django.core import validators
 from django.forms import MultiValueField, DecimalField, ChoiceField
 from moneyed.classes import Money
 
-from .widgets import MoneyWidget, CURRENCY_CHOICES
+from .widgets import MoneyWidget
+from ..settings import CURRENCY_CHOICES
 
 
 __all__ = ('MoneyField',)
@@ -17,17 +18,34 @@ class MoneyField(MultiValueField):
     if not hasattr(MultiValueField, 'empty_values'):
         empty_values = list(validators.EMPTY_VALUES)
 
-    def __init__(self, currency_widget=None, currency_choices=CURRENCY_CHOICES, choices=CURRENCY_CHOICES,
-                 max_value=None, min_value=None,
+    def __init__(self, currency_widget=None, currency_choices=CURRENCY_CHOICES,
+                 choices=CURRENCY_CHOICES, max_value=None, min_value=None,
                  max_digits=None, decimal_places=None, *args, **kwargs):
+
+        # choices does not make sense in this context, it would mean we had
+        # to replace two widgets with one widget dynamically... which is a
+        # mess. Instead, we let currency_choices be the same as choices and
+        # raise a warning.
         if currency_choices != CURRENCY_CHOICES:
             warn('currency_choices will be deprecated in favor of choices', PendingDeprecationWarning)
             choices = currency_choices
-        decimal_field = DecimalField(max_value, min_value, max_digits, decimal_places, *args, **kwargs)
-        choice_field = ChoiceField(choices=currency_choices)
-        self.widget = currency_widget if currency_widget else MoneyWidget(amount_widget=decimal_field.widget,
-                                                                          currency_widget=choice_field.widget)
-        fields = (decimal_field, choice_field)
+
+        amount_field = DecimalField(max_value, min_value, max_digits, decimal_places, *args, **kwargs)
+        currency_field = ChoiceField(choices=choices)
+
+        # TODO: No idea what currency_widget is supposed to do since it doesn't
+        # even receive currency choices as input. Somehow it's supposed to be
+        # instantiated from outside. Hard to tell.
+        if currency_widget:
+            self.widget = currency_widget
+        else:
+            self.widget = MoneyWidget(
+                amount_widget=amount_field.widget,
+                currency_widget=currency_field.widget
+            )
+
+        # The two fields that this widget comprises
+        fields = (amount_field, currency_field)
         super(MoneyField, self).__init__(fields, *args, **kwargs)
 
     def compress(self, data_list):
@@ -39,10 +57,13 @@ class MoneyField(MultiValueField):
         return None
 
     def _has_changed(self, initial, data):
+        # TODO: What on earth is going on here!?
+
         # ChoiceField._has_changed returns True always here, so we rely solely
         # on the DecimalField. Based on MultiValueField.
         if initial is None:
-            initial = ['' for x in range(0, len(data))]
+            initial = [''] * len(data)
+            # initial = ['' for x in range(0, len(data))]
         else:
             if not isinstance(initial, list):
                 initial = self.widget.decompress(initial)
