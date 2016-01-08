69fe02981b0ee8127607137400a723655d6b1f57
Greg Reinbach <greg@reinbach.com>
2016-01-08 12:34:10 -0500

    updated changelog and bumped version 0.7.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ class Tox(TestCommand):
 
 
 setup(name="django-money",
-      version="0.7.5",
+      version="0.7.6",
       description="Adds support for using money and currency fields in django models and forms. Uses py-moneyed as the money implementation.",
       url="https://github.com/jakewins/django-money",
       maintainer='Greg Reinbach',
