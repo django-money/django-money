8f687c60dee4e0b0e3a930653d86d19040e60eef
Dmitry Dygalo <dadygalo@gmail.com>
2016-03-20 09:24:41 +0100

    Improve test with assertion

--- tests/test_models.py
+++ tests/test_models.py
@@ -9,6 +9,7 @@ from decimal import Decimal
 from django import VERSION
 from django.core.exceptions import ValidationError
 from django.db.models import F, Q
+from django.utils.six import PY2
 
 import moneyed
 import pytest
@@ -430,4 +431,9 @@ def test_different_hashes():
 
 @pytest.mark.skipif(VERSION < (1, 7), reason='Django < 1.7 handles migrations differently')
 def test_migration_serialization():
-    MigrationWriter.serialize(MoneyPatched(100, 'GBP'))
+    imports = set(['import djmoney.models.fields'])
+    if PY2:
+        serialized = 'djmoney.models.fields.MoneyPatched(100, b\'GBP\')'
+    else:
+        serialized = 'djmoney.models.fields.MoneyPatched(100, \'GBP\')'
+    assert MigrationWriter.serialize(MoneyPatched(100, 'GBP')) == (serialized, imports)
