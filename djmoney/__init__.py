9a3dbbda6a22dfb2fda25e58e94b591e39e13c57
Dmitry Dygalo <dadygalo@gmail.com>
2016-07-07 16:53:09 +0200

    Fix for Django 1.7+

--- djmoney/__init__.py
+++ djmoney/__init__.py
@@ -1,11 +1,4 @@
 # -*- coding: utf-8 -*-
 
 __version__ = '0.9dev0'
-
-
-try:
-    from .contrib.django_rest_framework import register_money_field
-
-    register_money_field()
-except ImportError:
-    pass
+default_app_config = 'djmoney.apps.MoneyConfig'
