75294473d41417665d5024ddf7986666c11fc79b
Dmitry Dygalo <dadygalo@gmail.com>
2017-06-29 09:20:11 +0200

    More tests

--- tests/test_money.py
+++ tests/test_money.py
@@ -1,4 +1,8 @@
 # coding: utf-8
+import sys
+
+import pytest
+
 from djmoney.money import Money
 
 
@@ -8,3 +12,12 @@ def test_float():
 
 def test_repr():
     assert repr(Money('10.5', 'USD')) == '10 USD'
+
+
+def test_default_mul():
+    assert Money(10, 'USD') * 2 == Money(20, 'USD')
+
+
+@pytest.mark.skipif(sys.version_info[0] == 2, reason='py-moneyed doesnt support division on Python 2')
+def test_default_truediv():
+    assert Money(10, 'USD') / 2 == Money(5, 'USD')
