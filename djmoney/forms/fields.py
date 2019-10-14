6a2e3e13fa05856b9f1af6142dae990a0ce570ab
Benjamin Bach <benjamin@overtag.dk>
2019-10-14 11:58:01 +0200

    Remove "from future"

--- djmoney/forms/fields.py
+++ djmoney/forms/fields.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+
 
 from django.core.exceptions import ValidationError
 from django.forms import ChoiceField, DecimalField, MultiValueField
