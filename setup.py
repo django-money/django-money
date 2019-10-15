4be92af22010888a304b0635c557fafde1f415bd
Benjamin Bach <benjamin@overtag.dk>
2019-10-15 11:54:56 +0200

    Remove Python 2.7 support specified in setup.py and setup.cfg

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         'Django>=1.11',
         'py-moneyed>=0.8'
     ],
+    python_requires='>=3.5',
     platforms=['Any'],
     keywords=['django', 'py-money', 'money'],
     classifiers=[
@@ -65,8 +66,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
