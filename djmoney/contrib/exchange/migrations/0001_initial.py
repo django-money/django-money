96a51a7d8374be9ca04846739cf450bd6ea28504
Alexis Yushin <alexis@ww.net>
2021-04-24 15:18:52 +0200

    CURRENCY_TICKER_MAX_LENGTH -> CURRENCY_CODE_MAX_LENGTH

--- djmoney/contrib/exchange/migrations/0001_initial.py
+++ djmoney/contrib/exchange/migrations/0001_initial.py
@@ -2,7 +2,7 @@
 
 import django.db.models.deletion
 from django.db import migrations, models
-from djmoney.settings import CURRENCY_TICKER_MAX_LENGTH
+from djmoney.settings import CURRENCY_CODE_MAX_LENGTH
 
 class Migration(migrations.Migration):
 
@@ -16,14 +16,14 @@ class Migration(migrations.Migration):
             fields=[
                 ("name", models.CharField(max_length=255, primary_key=True, serialize=False)),
                 ("last_update", models.DateTimeField(auto_now=True)),
-                ("base_currency", models.CharField(max_length=CURRENCY_TICKER_MAX_LENGTH)),
+                ("base_currency", models.CharField(max_length=CURRENCY_CODE_MAX_LENGTH)),
             ],
         ),
         migrations.CreateModel(
             name="Rate",
             fields=[
                 ("id", models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
-                ("currency", models.CharField(max_length=CURRENCY_TICKER_MAX_LENGTH)),
+                ("currency", models.CharField(max_length=CURRENCY_CODE_MAX_LENGTH)),
                 ("value", models.DecimalField(decimal_places=6, max_digits=20)),
                 (
                     "backend",
