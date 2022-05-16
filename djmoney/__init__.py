4420cf906cd4102979c41c8ef475dc842a61ba08
Lutaaya Idris <lutaaya.idris@captiq.com>
2022-05-16 16:46:34 +0300

    Fix django app config default

--- djmoney/__init__.py
+++ djmoney/__init__.py
@@ -1,2 +1,10 @@
+import django
+
 __version__ = "2.1.1"
-default_app_config = "djmoney.apps.MoneyConfig"
+
+if django.VERSION >= (3, 2):
+    # The declaration is only needed for older Django versions
+    pass
+else:
+    default_app_config = "djmoney.apps.MoneyConfig"
+
