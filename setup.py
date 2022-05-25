90ea4ad266d40f2a249a9c8934339aa1011795b3
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2022-05-25 10:30:57 +0200

    Added testing for Django 4.1.

--- setup.py
+++ setup.py
@@ -79,6 +79,7 @@ setup(
         "Framework :: Django :: 2.2",
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
