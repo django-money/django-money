06128cc7c822df0e4175095908bdb9e98f52849e
László Károlyi <laszlo@karolyi.hu>
2023-04-20 11:26:09 +0200

    Fix typing and CI env

--- djmoney/money.pyi
+++ djmoney/money.pyi
@@ -1,15 +1,18 @@
 from __future__ import annotations
 
 from decimal import Decimal
-from typing import (
-    Any, Literal, Optional, TypedDict, Union, overload, type_check_only)
+from typing import Any, Optional, Union, overload, type_check_only
 
 from django.db.models.expressions import CombinedExpression, F
 from django.utils.safestring import SafeText
-from moneyed import Currency
-from moneyed import Money as DefaultMoney
+
+from typing_extensions import Literal, TypedDict
+
+from moneyed import Currency, Money as _DefaultMoney
+
 
 __all__ = ['Money', 'Currency']
+DefaultMoney = _DefaultMoney
 
 
 @type_check_only
@@ -31,37 +34,37 @@ class Money(DefaultMoney):
         self, *args, format_options: Optional[_FormatOptions] = ..., **kwargs
     ) -> None: ...
 
-    @overload
+    @overload  # type: ignore[override]
     def __add__(self, other: Money) -> Money: ...
     @overload
     def __add__(self, other: F) -> CombinedExpression: ...
-    @overload
+    @overload  # type: ignore[override]
     def __sub__(self, other: Money) -> Money: ...
     @overload
     def __sub__(self, other: F) -> CombinedExpression: ...
-    @overload
+    @overload  # type: ignore[override]
     def __mul__(self, other: _NumericalType) -> Money: ...
     @overload
     def __mul__(self, other: F) -> CombinedExpression: ...
-    @overload
+    @overload  # type: ignore[override]
     def __truediv__(self, other: _NumericalType) -> Money: ...
     @overload
     def __truediv__(self, other: F) -> CombinedExpression: ...
-    def __rtruediv__(self, other) -> None: ...
+    def __rtruediv__(self, other) -> None: ...  # type: ignore[override]
     @property
     def is_localized(self) -> bool: ...
     def __html__(self) -> SafeText: ...
     def __round__(self, n: Any | None = ...) -> Money: ...
-    def round(self, ndigits: int = ...) -> Money: ...
+    def round(self, ndigits: int = ...) -> Money: ...  # type: ignore[override]
     def __pos__(self) -> Money: ...
     def __neg__(self) -> Money: ...
     def __abs__(self) -> Money: ...
     def __rmod__(self, other) -> Money: ...
-    def __radd__(self, other) -> Money: ...
-    def __rmul__(self, other) -> Money: ...
+    def __radd__(self, other) -> Money: ...  # type: ignore[override]
+    def __rmul__(self, other) -> Money: ...  # type: ignore[override]
 
 
 def get_current_locale() -> str: ...
 
 
-_NumericalType = Union[Money, Decimal, float, int]
+_NumericalType = Union[Decimal, float, int]
