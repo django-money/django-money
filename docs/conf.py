85bce4e19ba0a1c7f558c0c988cf76ed17b1be7f
Benjamin Balder Bach <benjamin@overtag.dk>
2023-10-30 11:41:27 +0100

    Bump version, add release note about breaking change (#733)

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,11 @@ from djmoney import __version__
 # ones.
 extensions = [
     'sphinx.ext.viewcode',
+    'sphinx.ext.autosectionlabel',
 ]
+
+# Make sure the target is unique
+autosectionlabel_prefix_document = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
