fc2bf0cae77168a9be269f85f5939db9fe35bb03
Gert Van Gool <gertvangool@gmail.com>
2025-04-04 13:37:56 -0700

    Accept any signature for display_for_field

--- djmoney/admin.py
+++ djmoney/admin.py
@@ -1,4 +1,3 @@
-import django
 import django.contrib.admin.helpers as admin_helpers
 import django.contrib.admin.templatetags.admin_list as admin_list
 import django.contrib.admin.utils as admin_utils
@@ -12,12 +11,10 @@ MODULES_TO_PATCH = [admin_utils, admin_helpers, admin_list]
 def setup_admin_integration():
     original_display_for_field = admin_utils.display_for_field
 
-    def display_for_field(value, field, empty, avoid_link=False):
+    def display_for_field(value, field, *args, **kwargs):
         if isinstance(field, MoneyField):
             return str(value)
-        if django.VERSION < (5, 2):
-            return original_display_for_field(value, field, empty)
-        return original_display_for_field(value, field, empty, avoid_link=avoid_link)
+        return original_display_for_field(value, field,*args, **kwargs)
 
     for mod in MODULES_TO_PATCH:
         setattr(mod, "display_for_field", display_for_field)
